cab5ddd42765d778cd2f120f0c3b1bdf10ad4251
girishdev <girishkumar.aorg@gmail.com>
2018-10-10 11:50:33 +0530

    Added new Files at 11.43AM

---
+++ GitHub and GitLab.php
@@ -0,0 +1,109 @@
+GitHub and GitLab
+
+User Name:
+	girishkumar.aorg@gmail.com
+Password:
+	***girish****
+UserName:
+	girishdev
+
+To check Branch:
+	git branch
+
+git init
+git add testFile.php
+git add * // add all files
+
+git commit -m "first commit for testing purpose"
+git remote add origin https://github.com/girishdev/TestOct10.git
+git push -u origin master
+
+For Cloning:
+	sudo git clone https://github.com/girishdev/restaurant.git
+
+
+
+These are common Git commands used in various situations:
+
+start a working area (see also: git help tutorial)
+   clone      Clone a repository into a new directory
+   init       Create an empty Git repository or reinitialize an existing one
+
+work on the current change (see also: git help everyday)
+   add        Add file contents to the index
+   mv         Move or rename a file, a directory, or a symlink
+   reset      Reset current HEAD to the specified state
+   rm         Remove files from the working tree and from the index
+
+examine the history and state (see also: git help revisions)
+   bisect     Use binary search to find the commit that introduced a bug
+   grep       Print lines matching a pattern
+   log        Show commit logs
+   show       Show various types of objects
+   status     Show the working tree status
+
+grow, mark and tweak your common history
+   branch     List, create, or delete branches
+   checkout   Switch branches or restore working tree files
+   commit     Record changes to the repository
+   diff       Show changes between commits, commit and working tree, etc
+   merge      Join two or more development histories together
+   rebase     Reapply commits on top of another base tip
+   tag        Create, list, delete or verify a tag object signed with GPG
+
+collaborate (see also: git help workflows)
+   fetch      Download objects and refs from another repository
+   pull       Fetch from and integrate with another repository or a local branch
+   push       Update remote refs along with associated objects
+
+'git help -a' and 'git help -g' list available subcommands and some
+concept guides. See 'git help <command>' or 'git help <concept>'
+to read about a specific subcommand or concept.
+
+======================================
+https://www.quora.com/What-are-the-differences-between-GitHub-GitLab
+
+GitHub:
+   Bug tracking
+   Find your project easily
+   Meet new developers
+   Share your experience
+   Coordinate your projects together
+   Integrated code search
+   High compatibility
+
+GitHub and version control features: working with repositories, inspecting diffs, doing code review / pull requests, …
+
+GitLab:
+   GitLab exists in two forms.
+      - The first one is SAAS
+      - Individual solution GitLab Community Edition.
+   Free service
+   Open source code repository
+   Free hosting
+   Bug tracking mechanism
+   Files editing in the web interface
+   LDAP integration (Lightweight Directory Access Protocol)
+
+GitLab Offers a whole “platform” around code: continuous integration / deployments / devops, more advanced issue management, …
+
+GitLab benefits: reasons to use GitLab
+   Free service
+   Open source code repository
+   Free hosting
+   Bug tracking mechanism
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ newGitLabInfo.php
@@ -0,0 +1,4 @@
+https://about.gitlab.com/comparison/#gitlab-vs-github
+
+https://www.quora.com/What-are-some-major-differences-between-GitHub-and-GitLab-when-comparing-each-others-features-for-private-repositories-and-what-are-the-benefits-of-each
+
